627c03aa8c0d9f3e8cbe680077743767769e3fed
Alex Fabiano <fabianoa@gvsu.mail.edu>
2022-02-18 15:11:05 -0500

    Merge branch 'main' of https://github.com/owencairns/auto-maze-game350.git into main

--- mazeGame/src/module-info.java
+++ mazeGame/src/module-info.java
@@ -1,3 +1,27 @@
 module mazeGame {
-	//cuando el impostor es sos
+	public class maze {
+		
+		//generates the maze based on https://en.wikipedia.org/wiki/Maze_generation_algorithm
+		//int size = 8;
+		public static void mazeGen(int mapSize) {
+			
+			int[][] mazeMap = new int[mapSize][mapSize];
+			
+//			https://stackoverflow.com/questions/36939957/creating-a-matrix-in-java
+//			int max = 1;
+//		    int x = 0;
+//		    int y = 0;
+//
+//		    for (; x < mazeMap.length; x++) {
+//		        for (; y < mazeMap[x].length; y++) {
+//		            if (mazeMap[x][y] > max) {
+//		                max = mazeMap[x][y];
+//		                System.out.println(max);
+//		            }
+//		            System.out.println(mazeMap[x][y]);
+//		        }
+//		    }
+			
+		}
+	}
 }
