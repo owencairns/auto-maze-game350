a2168d48802d0e014ae5bf119b74ce772e27e684
Alex Fabiano <fabianoa@gvsu.mail.edu>
2022-02-22 21:27:14 -0500

    potential final class being added for conversion from matrix to gui

---
+++ mazeGame/src/mazeGame/generateMaze.java
@@ -0,0 +1,42 @@
+package mazeGame;
+
+
+
+public class generateMaze {
+	//generates the maze based on https://en.wikipedia.org/wiki/Maze_generation_algorithm
+	public int[][] mazeGen(int mapSize) {
+		int[][] mazeMap = new int[mapSize][mapSize];
+			
+		
+
+		return mazeMap;
+	}
+	public generateMaze() {
+		
+	}
+}
+
+/* ----------------------------------------------------------------------
+SOURCES
+https://stackoverflow.com/questions/36939957/creating-a-matrix-in-java
+https://en.wikipedia.org/wiki/Maze_generation_algorithm
+
+-------------------------------------------------------------------------
+SPARE CODE
+
+//			int max = 1;
+//		    int x = 0;
+//		    int y = 0;
+//
+//		    for (; x < mazeMap.length; x++) {
+//		        for (; y < mazeMap[x].length; y++) {
+//		            if (mazeMap[x][y] > max) {
+//		                max = mazeMap[x][y];
+//		                System.out.println(max);
+//		            }
+//		            System.out.println(mazeMap[x][y]);
+//		        }
+//		    }
+
+-------------------------------------------------------------------------
+*/
---
+++ mazeGame/src/mazeGame/mazeGame.java
@@ -0,0 +1,10 @@
+package mazeGame;
+
+public class mazeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
