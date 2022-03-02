4cb3a4598806ecbb8505cf6931f71b9f83514f4e
root <cairnso@mail.gvsu.edu>
2022-03-01 21:43:46 -0500

    rename doc

--- mazeGame/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"mazeGame","l":"generateMaze"},{"p":"mazeGame","l":"generateMazeTest"},{"p":"mazeGame","l":"Maze_GUI"},{"p":"mazeGame","l":"mazeGame"},{"p":"mazeGame","l":"player"}];updateSearchResults();
